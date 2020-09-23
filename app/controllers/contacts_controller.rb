076ae672e62faf903f1de5987a5aa372dc277308
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-09-23 20:24:58 +0900

    投稿者への確認メール送信機能の実装
    ・問い合わせ確認メールを投稿者へ送信する機能を仮実装し、動作確認を行いました。

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,8 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         if @contact.valid?(:create)
             if @contact.save
-                binding.pry
                 ContactMailer.notify_admin(@contact).deliver_now
+                ContactMailer.confirm_for_visitor(@contact).deliver_now
                 # redirect_to sended_contacts_path あとでこちらに
                 redirect_to root_path
             else
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -10,4 +10,13 @@ class ContactMailer < ApplicationMailer
         )
     end
 
+    def confirm_for_visitor(contact)
+        @contact = contact
+        mail(
+            from: ENV["EMAIL_ADDRESS_CONTACT"],
+            to: @contact.email,
+            subject: "(hrtg-ymzk.com)お問い合わせ頂きありがとうございます。"
+        )
+    end
+
 end
