df052932abccbda1d7db751c2c858ad32ad2b5de
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-09-25 21:23:33 +0900

    contacts関連のアクセス制御
    ・問い合わせフォームを正しい順序で扱えるように、sessionを利用して関連のアクセス制御設定を行いました。

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,11 @@
 class ContactsController < ApplicationController
 
+    before_action :redirect_to_root, only: [:create, :sended]
+    before_action :redirect_to_new, only: [:sended]
+
     def new
         @contact = Contact.new
+        session[:from_new] = "True"
     end
 
     def create
@@ -10,6 +14,7 @@ class ContactsController < ApplicationController
             if @contact.save
                 ContactMailer.notify_admin(@contact).deliver_now
                 ContactMailer.confirm_for_visitor(@contact).deliver_now
+                session[:from_create] = "True"
                 redirect_to sended_contacts_path
             else
                 render "contacts/new"
@@ -20,13 +25,29 @@ class ContactsController < ApplicationController
     end
 
     def sended
+        session.delete(:from_new)
+        session.delete(:from_create)
     end
 
+
     private
+
     def contact_params
         params.require(:contact).permit(
             :name, :email, :content
         )
+    end
+
+    def redirect_to_root
+        if session[:from_new] != "True"
+            redirect_to root_path
+        end
+    end
+
+    def redirect_to_new
+        if session[:from_create] != "True"
+            redirect_to new_contact_path
+        end
     end
 
 end
